3b29e23dc1e7b8b194c046707008be3d03182709
bio <marco_unterberger@gmx.at>
2024-03-22 17:15:13 +0100

    Update linux_lib.sh

--- linux_lib.sh
+++ linux_lib.sh
@@ -2,3 +2,13 @@
 # SPDX-License-Identifier: MIT
 
 set -u
+
+# log message
+# $1 message text
+log_message() {
+
+   log=${1:-"no text"}
+   printf "\n$(date +%Y-%m-%d-%H-%M-%S): ${log}\n"
+   printf "########################################\n"
+
+}
