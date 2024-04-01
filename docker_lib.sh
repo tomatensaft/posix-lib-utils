da5edb12c23740ef95299635012530eeafea8036
tomatensaft <marco_unterberger@gmx.at>
2024-04-01 21:35:36 +0200

    global root path

--- docker_lib.sh
+++ docker_lib.sh
@@ -3,11 +3,20 @@
 
 # set -x
 
-# include external libs from git module
-if [ -f  ./posix-lib-utils/standard_lib.sh ]; then
-    . ./posix-lib-utils/standard_lib.sh
+# check if script root path is set from main script
+if [ ! -z ${SCRIPT_ROOT_PATH} ]; then
+	printf "$0: script root path set - use ${SCRIPT_ROOT_PATH} for include\n"
 else
-    printf "$0: standardlib not found - exit."
+	printf "$0: script rootpath ${SCRIPT_ROOT_PATH} not found - use local\n"
+	SCRIPT_ROOT_PATH=./
+fi
+
+# include external libs from git module
+if [ -f  ${SCRIPT_ROOT_PATH}/posix-lib-utils/standard_lib.sh ]; then
+    printf "$0: include script from ${SCRIPT_ROOT_PATH}\n"
+    . ${SCRIPT_ROOT_PATH}/posix-lib-utils/standard_lib.sh
+else
+    printf "$0: standardlib not found - exit\n"
     exit 1
 fi
 
